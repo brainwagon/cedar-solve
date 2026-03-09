5a18437f1d20a2ae213fe48e22410096367feb6d
MvandeWettering <mvandewettering@gmail.com>
2026-03-09 00:59:35 -0700

    Rename tetra3 to cedar_solve

---
+++ cedar_solve/__init__.py
@@ -0,0 +1,5 @@
+name = "cedar_solve"
+
+from .cedar_solve import Tetra3, get_centroids_from_image, crop_and_downsample_image
+
+__all__ = ['Tetra3', 'get_centroids_from_image', 'crop_and_downsample_image']
